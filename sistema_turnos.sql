93012b6d32fa4b56fd60c096a38bf2e29c953d1b
Rodrigo Funes <67166664+Rodro560@users.noreply.github.com>
2025-06-08 12:39:41 -0300

    Agrego el rol de la tabla usuario

--- sistema_turnos.sql
+++ sistema_turnos.sql
@@ -360,6 +360,8 @@ CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `contrasena` varchar(255) NOT NULL,
+  `rol` varchar(20) NOT NULL,
+  
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
