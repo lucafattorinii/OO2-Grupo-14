5e2518731fb85485a9a78d1d4864596f0f69e1eb
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-06-08 19:44:17 -0300

    Agrego una lista segun el servicio para disponibilidad

--- src/main/java/com/grupo14/turnos/repository/DisponibilidadRepository.java
+++ src/main/java/com/grupo14/turnos/repository/DisponibilidadRepository.java
@@ -1,7 +1,11 @@
 package com.grupo14.turnos.repository;
 
 import com.grupo14.turnos.modelo.Disponibilidad;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DisponibilidadRepository extends JpaRepository<Disponibilidad, Integer> {
+	List<Disponibilidad> findByServiciosId(Integer servicioId);
 }
