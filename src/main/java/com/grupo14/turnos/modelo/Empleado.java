487a810e9ed37ca59d6c6769979faba9572ca78f
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-06-11 16:45:01 -0300

    Agregando lombok y corrigiendo relaciones

--- src/main/java/com/grupo14/turnos/modelo/Empleado.java
+++ src/main/java/com/grupo14/turnos/modelo/Empleado.java
@@ -1,13 +1,15 @@
 package com.grupo14.turnos.modelo;
 
 import jakarta.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
-
+import lombok.*;
 
 @Entity
 @Table(name = "empleado")
 @PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Empleado extends Persona {
 
     @Column(name = "cuit")
@@ -19,105 +21,24 @@ public class Empleado extends Persona {
     @Column(name = "puesto_cargo", length = 100)
     private String puestoCargo;
 
-   
-    @OneToMany(
-        mappedBy = "empleado",
-        cascade = CascadeType.ALL,
-        orphanRemoval = true,
-        fetch = FetchType.LAZY
+    // Un empleado pertenece a UN servicio
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "servicio_id",
+        referencedColumnName = "id_servicio",
+        foreignKey = @ForeignKey(name = "empleado_servicio_fk")
     )
-    private Set<Disponibilidad> disponibilidades = new HashSet<>();
+    private Servicio servicio;
 
-    
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-        name = "servicio_empleado",
-        joinColumns = @JoinColumn(
-            name = "empleado_id",
-            referencedColumnName = "id",
-            foreignKey = @ForeignKey(name = "servicio_empleado_empleado_fk")
-        ),
-        inverseJoinColumns = @JoinColumn(
-            name = "servicio_id",
-            referencedColumnName = "id_servicio",
-            foreignKey = @ForeignKey(name = "servicio_empleado_servicio_fk")
-        )
-    )
-    private Set<Servicio> servicios = new HashSet<>();
-
-   
-    public Empleado() { }
-
-    public Empleado(String email, String contrasena, String rol,
+    // Constructor que incluye atributos del padre y el servicio
+    public Empleado(String email, String contrasena, Rol rol,
                     String nombre, String apellido, Long dni,
-                    Long cuit, Integer legajo, String puestoCargo) {
+                    Long cuit, Integer legajo, String puestoCargo,
+                    Servicio servicio) {
         super(email, contrasena, rol, nombre, apellido, dni);
         this.cuit = cuit;
         this.legajo = legajo;
         this.puestoCargo = puestoCargo;
-    }
-
-   
-    // getters y setters
-
-    public Long getCuit() {
-        return cuit;
-    }
-
-    public void setCuit(Long cuit) {
-        this.cuit = cuit;
-    }
-
-    public Integer getLegajo() {
-        return legajo;
-    }
-
-    public void setLegajo(Integer legajo) {
-        this.legajo = legajo;
-    }
-
-    public String getPuestoCargo() {
-        return puestoCargo;
-    }
-
-    public void setPuestoCargo(String puestoCargo) {
-        this.puestoCargo = puestoCargo;
-    }
-
-    public Set<Disponibilidad> getDisponibilidades() {
-        return disponibilidades;
-    }
-
-    public void setDisponibilidades(Set<Disponibilidad> disponibilidades) {
-        this.disponibilidades = disponibilidades;
-    }
-
-    public Set<Servicio> getServicios() {
-        return servicios;
-    }
-
-    public void setServicios(Set<Servicio> servicios) {
-        this.servicios = servicios;
-    }
-
- 
-    public void addDisponibilidad(Disponibilidad disp) {
-        disponibilidades.add(disp);
-        disp.setEmpleado(this);
-    }
-
-    public void removeDisponibilidad(Disponibilidad disp) {
-        disponibilidades.remove(disp);
-        disp.setEmpleado(null);
-    }
-
-    public void addServicio(Servicio servicio) {
-        servicios.add(servicio);
-        servicio.getEmpleados().add(this);
-    }
-
-    public void removeServicio(Servicio servicio) {
-        servicios.remove(servicio);
-        servicio.getEmpleados().remove(this);
+        this.servicio = servicio;
     }
 }
