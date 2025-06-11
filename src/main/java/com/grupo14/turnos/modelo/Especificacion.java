563a0fa8733553c8763c0dbe463d5321eb971615
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-06-11 18:09:58 -0300

    Agrego lombok

--- src/main/java/com/grupo14/turnos/modelo/Especificacion.java
+++ src/main/java/com/grupo14/turnos/modelo/Especificacion.java
@@ -1,16 +1,19 @@
 package com.grupo14.turnos.modelo;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Entity
 @Table(name = "especificacion")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Especificacion {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private long id; // Este ID se compartirá con el ID de Servicio
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(
@@ -27,75 +30,27 @@ public class Especificacion {
     @Column(name = "detalles", columnDefinition = "TEXT")
     private String detalles;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId // Compartir el mismo ID con la entidad Servicio
     @JoinColumn(
-        name = "servicio_id",
-        referencedColumnName = "id_servicio",
-        unique = true,
-        nullable = false,
+        name = "id",
+        referencedColumnName = "id_servicio", // clave primaria de Servicio
         foreignKey = @ForeignKey(name = "especificacion_servicio_fk")
     )
     private Servicio servicio;
 
-
-    public Especificacion() { }
-
-    public Especificacion(Direccion direccion, Rubro rubro,
-                         String detalles, Servicio servicio) {
+    @Builder
+    public Especificacion(Direccion direccion, Rubro rubro, String detalles, Servicio servicio) {
         this.direccion = direccion;
         this.rubro = rubro;
         this.detalles = detalles;
         this.setServicio(servicio);
     }
 
-
-    // getters y setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Direccion getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(Direccion direccion) {
-        this.direccion = direccion;
-    }
-
-    public Rubro getRubro() {
-        return rubro;
-    }
-
-    public void setRubro(Rubro rubro) {
-        this.rubro = rubro;
-    }
-
-    public String getDetalles() {
-        return detalles;
-    }
-
-    public void setDetalles(String detalles) {
-        this.detalles = detalles;
-    }
-
-    public Servicio getServicio() {
-        return servicio;
-    }
-
     public void setServicio(Servicio servicio) {
         this.servicio = servicio;
-        /*if (servicio != null && servicio.getEspecificacion() == null) {
-            servicio.setEspecificacion(this);
-        }*/
+        if (servicio != null) {
+            this.id = servicio.getId();
+        }
     }
-
-	public void setIdDireccion(@NotNull Integer direccionId) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
