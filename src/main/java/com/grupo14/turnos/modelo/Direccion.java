7d990ccad80853bcad6849f6bd0261b4e9a07717
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-06-11 18:08:49 -0300

    Agrego lombok

--- src/main/java/com/grupo14/turnos/modelo/Direccion.java
+++ src/main/java/com/grupo14/turnos/modelo/Direccion.java
@@ -1,9 +1,14 @@
 package com.grupo14.turnos.modelo;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "direccion")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Direccion {
 
     @Id
@@ -29,11 +34,8 @@ public class Direccion {
     @Column(name = "codigo_postal", length = 10)
     private String codigoPostal;
 
-    
-    // Constructores
-    
-    public Direccion() { }
-
+    // Constructor con todos los campos excepto idDireccion (que es generado)
+    @Builder
     public Direccion(String pais, String provincia, String ciudad,
                      String calle, String numeroCalle, String codigoPostal) {
         this.pais = pais;
@@ -41,65 +43,6 @@ public class Direccion {
         this.ciudad = ciudad;
         this.calle = calle;
         this.numeroCalle = numeroCalle;
-        this.codigoPostal = codigoPostal;
-    }
-
-   
-    // Getters / Setters
-   
-    public Integer getIdDireccion() {
-        return idDireccion;
-    }
-
-    public void setIdDireccion(Integer idDireccion) {
-        this.idDireccion = idDireccion;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    public String getCalle() {
-        return calle;
-    }
-
-    public void setCalle(String calle) {
-        this.calle = calle;
-    }
-
-    public String getNumeroCalle() {
-        return numeroCalle;
-    }
-
-    public void setNumeroCalle(String numeroCalle) {
-        this.numeroCalle = numeroCalle;
-    }
-
-    public String getCodigoPostal() {
-        return codigoPostal;
-    }
-
-    public void setCodigoPostal(String codigoPostal) {
         this.codigoPostal = codigoPostal;
     }
 }
