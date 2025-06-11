9479e7dde3a919294e6be03702bb3c9e2274d671
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-06-11 16:47:37 -0300

    agregando lombok

--- src/main/java/com/grupo14/turnos/modelo/Usuario.java
+++ src/main/java/com/grupo14/turnos/modelo/Usuario.java
@@ -1,16 +1,18 @@
 package com.grupo14.turnos.modelo;
 
 import jakarta.persistence.*;
-
+import lombok.*;
 
 @Entity
 @Table(name = "usuario")
 @Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+@NoArgsConstructor
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     protected Integer id;
 
     @Column(name = "email", length = 100, unique = true, nullable = false)
@@ -18,51 +20,14 @@ public class Usuario {
 
     @Column(name = "contrasena", length = 255, nullable = false)
     private String contrasena;
-    
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "rol", length = 20, nullable = false)
-    private String rol;
+    private Rol rol;
 
-    
-    public Usuario() { }
-
-    public Usuario(String email, String contrasena, String rol) {
+    public Usuario(String email, String contrasena, Rol rol) {
         this.email = email;
         this.contrasena = contrasena;
-        this.rol=rol;
+        this.rol = rol;
     }
-
-    
- // getters y setters
-    
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-	public String getRol() {
-		return rol;
-	}
-
-	public void setRol(String rol) {
-		this.rol = rol;
-	}
 }
