2dd57048b4b917a6b8b14841ec52653aaa41e34a
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-06-15 18:22:41 -0300

    Corrigo un error en el repositori

--- src/main/java/com/grupo14/turnos/repository/TurnoRepository.java
+++ src/main/java/com/grupo14/turnos/repository/TurnoRepository.java
@@ -7,25 +7,20 @@ import com.grupo14.turnos.modelo.Fecha;
 import com.grupo14.turnos.modelo.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 public interface TurnoRepository extends JpaRepository<Turno, Long> {
-
-    List<Turno> findByEstado(String estado);
-
+	
+	List<Turno> findByEstado(String estado);
     List<Turno> findByEstado(EstadoTurno estado);
-
     List<Turno> findByClienteId(Long clienteId);
-
-    boolean existsByFechaAndHoraAndDisponibilidadAndFecha_Direccion(
-        Fecha fecha,
-        LocalTime hora,
-        Disponibilidad disponibilidad,
-        Direccion direccion
-    );
-
     
-    boolean existsByDisponibilidadIdAndFechaAndHora(Long disponibilidadId, LocalDate fecha, LocalTime hora);
+    boolean existsByFechaAndHoraAndDisponibilidadAndFecha_Direccion(
+    	    Fecha fecha,
+    	    LocalTime hora,
+    	    Disponibilidad disponibilidad,
+    	    Direccion direccion
+    	);
+
 }
