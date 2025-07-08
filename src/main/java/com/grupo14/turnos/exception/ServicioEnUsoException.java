e4ece98b398e86841b4d891d7c7908824c85b3ff
Bono-Dev929 <bono.dev.22@gmail.com>
2025-07-08 20:01:34 -0300

    Se agrego la excepcion para no eliminar servicios con turnos asignados

---
+++ src/main/java/com/grupo14/turnos/exception/ServicioEnUsoException.java
@@ -0,0 +1,8 @@
+package com.grupo14.turnos.exception;
+
+public class ServicioEnUsoException extends RuntimeException {
+
+	public ServicioEnUsoException(String mensaje) {
+        super(mensaje);
+    }
+}
