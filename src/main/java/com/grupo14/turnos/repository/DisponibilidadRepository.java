b603662551ddfa4772d1ff4e149ca8240d094ea9
Rodro <Rodro@DESKTOP-6CLD8V8>
2025-07-09 14:26:03 -0300

    Creando un error para las disponibildades duplicadas

--- src/main/java/com/grupo14/turnos/repository/DisponibilidadRepository.java
+++ src/main/java/com/grupo14/turnos/repository/DisponibilidadRepository.java
@@ -1,13 +1,20 @@
 package com.grupo14.turnos.repository;
 
+import com.grupo14.turnos.modelo.DiaSemana;
 import com.grupo14.turnos.modelo.Disponibilidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public interface DisponibilidadRepository extends JpaRepository<Disponibilidad, Long> {
 	List<Disponibilidad> findByServicios_IdServicio(Long servicioId);
 	@Query("SELECT DISTINCT d FROM Disponibilidad d LEFT JOIN FETCH d.servicios")
 	List<Disponibilidad> findAllWithServicios();
+	
+	
+    boolean existsByServicios_IdServicioAndDiaSemanaAndHoraInicioAndHoraFin(
+            Long servicioId, DiaSemana diaSemana, LocalTime horaInicio, LocalTime horaFin);
+
 }
